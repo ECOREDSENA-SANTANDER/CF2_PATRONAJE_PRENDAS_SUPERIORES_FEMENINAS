68806ce22e53fd1475d73d5677965e6275cd77e9
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-07-02 09:27:54 -0500

    Cargue del CFA

--- src/config/global.js
+++ src/config/global.js
@@ -145,7 +145,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF2_52450478_DU.pdf',
       },
       {
         icono: 'fas fa-download',
