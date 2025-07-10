ebeb1b71a70bdac686fd10912188920904520ea2
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-07-09 23:05:49 -0500

    Punto

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -409,7 +409,7 @@
           .col
             .row.justify-content-between.align-items-center
               .col.mb-3.mb-sm-0
-                h4.mb-1 Se invita a explorar el PDF - Interpretación de simbologías técnicas y reconocimiento de la estructura corporal en procesos de patronaje y corte
+                h4.mb-1 Se invita a explorar el PDF - Interpretación de simbologías técnicas y reconocimiento de la estructura corporal en procesos de patronaje y corte.
               .col-sm-auto
                 a.boton.color-acento-botones.texto-blanco(:href="obtenerLink('/downloads/3_Anexo.pdf')" target="_blank")
                   span Descargar
