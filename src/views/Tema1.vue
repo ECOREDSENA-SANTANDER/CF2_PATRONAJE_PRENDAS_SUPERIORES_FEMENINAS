b04757e28c2c8ccf8e2016749c8eea188f9b8eb4
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-10-03 08:48:47 -0500

    Avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -172,7 +172,7 @@
             | Patronaje sobre maniquí 
           figure
             img.mb-2(src='@/assets/curso/t1/fig5.png', alt='Proceso de construcción de una prenda directamente sobre un maniquí. Se trabaja con una pieza de tela fijada con alfileres, marcando líneas guía que siguen las proporciones del cuerpo.')
-          figcaption Fuente: https://skarlett.es/algunos-consejos-para-embarcarse-por-primera-vez-en-la-tecnica-del-modelaje-en-el-diseno-de-moda/
+          figcaption Fuente: https://www.freepik.es/foto-gratis/mujer-crea-patrones-ropa-maniquies_15970721.htm#fromView=search&page=1&position=14&uuid=326d4dae-c7e9-475a-b876-514a3acf71ac&query=patronaje+maniqui
 
       .row.justify-content-center.align-items-center.mb-4.mt-4(data-aos="fade-right")
         .col-lg-4.col-md-6.col-6
